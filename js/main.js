89f3c8bf91bdb2b7460b8052f4b22259e0f63a9c
Kristina <kzatesova@gmail.com>
2022-07-19 12:17:02 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -4,17 +4,12 @@ var modal = document.getElementById("myModal");
 
 var btn = document.getElementById("myBtn");
 
-var btn2 = document.getElementById("myBtn2");
-
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
   modal.style.display = "block";
 };
 
-btn2.onclick = function () {
-    modal.style.display = "block";
-  };
 
 span.onclick = function () {
   modal.style.display = "none";
